e9569242639a5fabdf983292987716c444e2776e
taehyeki <gmrrltkxogus@naver.com>
2021-12-29 17:23:01 +0900

    login middleware

--- login_join/test.py
+++ login_join/test.py
@@ -13,37 +13,58 @@ db = client.dbProject1
 app = Flask(__name__)
 
 
-def home():
+
+
+# 로그인 했는지 안했는지 확인.
+def sign_in():
     token_receive = request.cookies.get('token')
     try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"user_id": payload['id']})
-        return user_info
+        jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return 'ok'
+    except jwt.ExpiredSignatureError:
+        return 'bad'
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return 'bad'
 
+#현재 로그인 된 유저의 정보를 return
+def get_user():
+    token_receive = request.cookies.get('token')
+    user_info =  jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user = db.user.find_one({'user_id':user_info['id']},{'_id':0})
+    print(user)
 
-
-
+#처음 /에 접속시 로그인 안되어있으면 /로 이동, 로그인 되어있으면 /home으로 이동
 @app.route('/')
-def hello():
+def init():
     token_receive = request.cookies.get('token')
     try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"user_id": payload['id']})
-        return render_template('index.html', user=user_info)
+        jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return redirect(url_for('home'))
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login_page"))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login_page"))
 
 
-
+#로그인 페이지
 @app.route('/login')
 def login_page():
+    #로그인 되어 있다면 홈으로 이동
+    logged = sign_in()
+    if logged == 'ok':
+        return redirect(url_for('home'))
     return render_template('lol.html')
+
+#회원가입 페이지
 @app.route('/join')
 def join_page():
+    #로그인 되어 있다면 홈으로 이동
+    logged = sign_in()
+    if logged == 'ok':
+        return redirect(url_for('home'))
     return render_template('join.html')
 
+#로그인 api
 @app.route('/api/login',methods=['POST'])
 def login():
     data = request.json
@@ -57,10 +78,22 @@ def login():
     print(is_it['user_id'])
     payload = {
         'id' : is_it['user_id'],
-
+        
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     return jsonify({'result': 'success', 'token': token})
+
+# 홈 화면
+@app.route('/home')
+def home():
+    # 로그인 되어있지 않으면 로그인창으로 보냄
+    logged = sign_in()
+    if logged == 'bad':
+        return redirect(url_for('login_page'))
+    user = get_user()
+    print(user)
+    return render_template('home.html')
+
 
 
 # @app.route('/login/github/callback')
